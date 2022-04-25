fd9a24c3e7ed0b082fc296244bff685781eb0dc2
Digalv <daniil.tabakov004@gmail.com>
2022-04-25 07:41:52 +0200

    1.5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,12 +17,12 @@ public class Main {
         for (int i = 0; i < items.length; i++) {
             try {
                 items[i] = new Item("Item " + (i + 1), (float) (Math.random() * 99 + 1), (int) (Math.random() * 10 + 1), "Digalv");
-                if ((items[i].getDataLife().getDayOfMonth() - items[i].getDataProduction().getDayOfMonth()) == 2) {
-                    System.out.println(items[i].toString());
-                }
             } catch (Exception e) {
                 System.out.println("Вы ввели некоректные значения, попробуйте еще раз!");
                 break;
+            }
+            if ((items[i].getDataLife().getDayOfMonth() - items[i].getDataProduction().getDayOfMonth()) == 2) {
+                System.out.println(items[i].toString());
             }
         }
     }
@@ -36,18 +36,18 @@ public class Main {
         for (int i = 0; i < comp.length; i++) {
             try {
                 comp[i] = new Computer(DataBaseComputerParts.getRandomBrandName(), DataBaseComputerParts.getRandomModelName(), DataBaseComputerParts.getRandomCpuName(), (int) (Math.random() * 11 + 2), DataBaseComputerParts.getRandomStorage(), (int) (Math.random() * 501 + 1000));
-                sum_price += comp[i].getPrice();
             } catch (Exception e) {
                 System.out.println("Произошла ошибка, проверьте данные: ");
                 System.exit(0);
             }
+            sum_price += comp[i].getPrice();
         }
 
         System.out.println("Avg= " + sum_price / comp.length);
 
-        for (int i = 0; i < comp.length; i++) {
-            if (comp[i].getPrice() > sum_price / comp.length) {
-                comp[i].getInfo();
+        for (Computer computer : comp) {
+            if (computer.getPrice() > sum_price / comp.length) {
+                computer.getInfo();
             }
         }
     }
